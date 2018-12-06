7ce99dc41b82d93efb6a3d7d048a7dc87d4d0253
emipa606 <emipa606@users.noreply.github.com>
2018-12-06 22:36:42 +0100

    Add files via upload

---
+++ Source/RefugeeChased/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyConfiguration("")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: AssemblyCompany("")]
+[assembly: CompilationRelaxations(8)]
+[assembly: AssemblyTitle("RefugeeChased")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("RefugeeChased")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
